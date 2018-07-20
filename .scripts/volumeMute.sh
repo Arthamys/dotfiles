ce2945260725d564c0fe8247a7eb393004b21705
Galilee 'Bill' Enguehard <galilee.enguehard@gmail.com>
2018-07-20 13:28:07 +0200

    Volume scripts target running sink

--- .scripts/volumeMute.sh
+++ .scripts/volumeMute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SINK=$(pactl list sinks | head -n 1 | awk '{ print $NF }'  |sed 's/#//')
+SINK=$(pactl list sinks | grep -B 1 RUNNING | head -n 1 | awk '{ print $NF }' | sed 's/#//')
 
 pactl set-sink-mute $SINK toggle
