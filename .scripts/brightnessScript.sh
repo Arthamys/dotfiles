08d4aec54d062d914379d3d525727ad852c4b3d1
Galilée 'Bill' Enguehard <galilee.enguehard@gmail.com>
2020-01-25 02:07:19 +0100

    Make brightness script bottom out to luminosity 1

--- .scripts/brightnessScript.sh
+++ .scripts/brightnessScript.sh
@@ -5,6 +5,11 @@ if [ $1 -gt 0 ]; then
   light -A $1
 else
   # light -U decreases brightness
-  # Only accepts positive numbers, so invert input
-  light -U $(echo "$1 * -1" | bc)
+  lum=$(light -G | cut -d '.' -f 1)
+  if [ $(($lum + $1)) -le 0 ]; then
+    light -S 1
+  else
+    # Only accepts positive numbers, so invert input
+    light -U $(echo "$1 * -1" | bc)
+  fi
 fi
