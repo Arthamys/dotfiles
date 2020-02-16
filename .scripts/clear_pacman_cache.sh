9eac21862a44bc1f4d2daacf8c1428b9e678a256
Galilée 'Bill' Enguehard <galilee.enguehard@gmail.com>
2020-02-16 20:19:55 +0100

    Cleanup pacman cache timer

---
+++ .scripts/clear_pacman_cache.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+## change output of `pacman -Qe` to be `head "-" tail "*"`
+## list all files in cache except those that match this list
+## and delete them
+latest_versions=$(pacman -Qe | perl -pe 's|(.*?) (.*?)\n|\1-\2-\*\||')
+cd /var/cache/pacman/pkg
+outdated_versions=$(exa -I "$latest_versions")
+rm $outdated_versions
--- .scripts/lock_screen.sh
+++ .scripts/lock_screen.sh
@@ -5,4 +5,4 @@
 #/usr/bin/i3lock --pointer=win -i /tmp/blurred.png -b
 #rm /tmp/lock.png /tmp/blurred.png
 
-i3lock -i /home/san/.wallpapers/1518715279450.png
+i3lock -i /home/san/.wallpapers/alena_highway_sunset_FHD_4K.png
