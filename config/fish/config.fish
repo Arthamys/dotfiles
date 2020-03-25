213722d021cdf58b481d2c6c1398303e1d44dfab
Galilée 'Bill' Enguehard <galilee.enguehard@gmail.com>
2020-03-25 17:15:33 +0100

    Remove `TODOs` section from fish fish greetings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -179,45 +179,4 @@ function fish_greeting
 			sed 's/^/\t/' \
 		)
 	echo
-
-	set r (random 0 100)
-	if [ $r -lt 5 ] # only occasionally show backlog (5%)
-		echo -e " \e[1mBacklog\e[0;32m"
-		set_color blue
-		echo "  [project] <description>"
-		echo
-	end
-
-	set_color normal
-	echo -e " \e[1mTODOs\e[0;32m"
-	echo
-	if [ $r -lt 10 ]
-		# unimportant, so show rarely
-		set_color cyan
-		# echo "  [project] <description>"
-	end
-	if [ $r -lt 25 ]
-		# back-of-my-mind, so show occasionally
-		set_color green
-		# echo "  [project] <description>"
-	end
-	if [ $r -lt 50 ]
-		# upcoming, so prompt regularly
-		set_color yellow
-		# echo "  [project] <description>"
-	end
-
-	# urgent, so prompt always
-	set_color red
-	# echo "  [project] <description>"
-
-	echo
-
-	if test -s ~/todo
-		set_color magenta
-		cat todo | sed 's/^/ /'
-		echo
-	end
-
-	set_color normal
 end
