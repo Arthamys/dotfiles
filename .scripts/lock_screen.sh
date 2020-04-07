c102c3adeeb1a2f7867bd4719408722ae545149a
Galilée 'Bill' Enguehard <galilee.enguehard@gmail.com>
2020-04-07 15:15:38 +0200

    Update wallpaper

--- .scripts/lock_screen.sh
+++ .scripts/lock_screen.sh
@@ -5,4 +5,4 @@
 #/usr/bin/i3lock --pointer=win -i /tmp/blurred.png -b
 #rm /tmp/lock.png /tmp/blurred.png
 
-i3lock -i /home/san/.wallpapers/alena_highway_sunset_FHD_4K.png
+i3lock -i /home/san/Pictures/japan_doorway_pixelart.png
