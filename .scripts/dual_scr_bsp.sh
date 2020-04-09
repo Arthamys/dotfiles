a0cedf155291d3913e8aafff2c0740bac28e7f40
Galilée 'Bill' Enguehard <galilee.enguehard@gmail.com>
2020-04-09 20:15:38 +0200

    Fix invalid condition in 2monitor bsp start script

--- .scripts/dual_scr_bsp.sh
+++ .scripts/dual_scr_bsp.sh
@@ -6,6 +6,6 @@ max_monitor_res=$(xrandr -q | grep -A1 -w connected | cut -d ' ' -f4 | tail -n 1
 # NOTE This is a simplistic approach because I already know the settings I
 # want to apply.
 my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
-if [[ $my_laptop_external_monitor = *connected* ]]; then
+if [[ $my_laptop_external_monitor = connected* ]]; then
 xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output HDMI-1 --mode "$max_monitor_res" --rotate normal --right-of eDP-1
 fi
